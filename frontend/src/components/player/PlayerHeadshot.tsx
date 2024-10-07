8e2a95131ffc35bdb6cade26e7a0f9bdae8dac91
Lambert Liu <lambert.liu@uwaterloo.ca>
2024-10-07 18:19:19 -0400

    try rendering smaller img for main table

--- frontend/src/components/player/PlayerHeadshot.tsx
+++ frontend/src/components/player/PlayerHeadshot.tsx
@@ -15,13 +15,19 @@ const PlayerHeadshot = ({ player, size }: PlayerHeadshotProps) => {
 
   const width = size === "sm" ? small : size === "md" ? medium : large;
   const height = Math.round(width * proportion);
-
   const widthClass = size === "sm" ? "w-11" : size === "md" ? "w-16" : "w-28";
+
+  const headshotSrc =
+    size === "sm" &&
+    player.headshot !==
+      "https://secure.espncdn.com/combiner/i?img=/i/headshots/nophoto.png"
+      ? `https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/${player.id}.png&w=96&h=70&cb=1`
+      : player.headshot;
 
   return (
     <Image
       className={cn(widthClass, "relative h-auto")}
-      src={player.headshot}
+      src={headshotSrc}
       alt={player.firstName + " " + player.lastName}
       width={width}
       height={height}
